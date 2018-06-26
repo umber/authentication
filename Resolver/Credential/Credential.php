163ff4a83bc52046f4902a1a0eb6c5ea168d4671
Matthew Usurp <matt.usurp@gmail.com>
2018-06-26 15:52:55 +0100

    Broke out the authorisation roles and permissions.
    Moved authorisation roles and permissions from the user interface to
    a dedicated interface. The user interface now extends this.

---
+++ Resolver/Credential/Credential.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Umber\Authentication\Resolver\Credential;
+
+use Umber\Authentication\Prototype\Authorisation\AuthorisationAwareInterface;
+
+/**
+ * {@inheritdoc}
+ */
+final class Credential implements CredentialInterface
+{
+    private $aware;
+
+    public function __construct(AuthorisationAwareInterface $aware)
+    {
+        $this->aware = $aware;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthorisationRoles(): array
+    {
+        return $this->aware->getAuthorisationRoles();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthorisationPermissions(): array
+    {
+        return $this->aware->getAuthorisationPermissions();
+    }
+}

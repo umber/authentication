f992e42490646bb215bef01e4ca91a892d00730c
Matthew Usurp <matt.usurp@gmail.com>
2019-05-01 16:13:50 +0100

    Allowing previous exceptions to be passed.

--- src/Umber/Authentication/Exception/Resolver/CannotResolveAuthenticationMethodException.php
+++ src/Umber/Authentication/Exception/Resolver/CannotResolveAuthenticationMethodException.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Umber\Authentication\Exception\Resolver;
 
 use Exception;
+use Throwable;
 
 /**
  * An exception that is thrown when the resolver fails for the authentication method.
@@ -14,15 +15,15 @@ final class CannotResolveAuthenticationMethodException extends Exception
     /**
      * @return CannotResolveAuthenticationMethodException
      */
-    public static function create(): self
+    public static function create(?Throwable $previous = null): self
     {
-        return new self();
+        return new self($previous);
     }
 
-    public function __construct()
+    public function __construct(?Throwable $previous = null)
     {
         $message = 'The authentication method provided did not resolve.';
 
-        parent::__construct($message);
+        parent::__construct($message, 0, $previous);
     }
 }
